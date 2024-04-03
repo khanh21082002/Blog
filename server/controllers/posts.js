8611b4b8f1b47a414a0a847aa5523db79378709a
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2024-04-03 18:32:26 +0700

    pagging

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,12 +1,34 @@
 import { PostModel } from "../models/PostModel.js";
 
 export const getPosts = async (req, res) => {
+    const page = parseInt(req.query.page) || 1; // Lấy trang từ yêu cầu, mặc định là trang 1
+    const limit = parseInt(req.query.limit) || 6; // Lấy số lượng bài viết trên mỗi trang từ yêu cầu, mặc định là 10 bài viết
+
+    const startIndex = (page - 1) * limit;
+
     try {
-        const posts = await PostModel.find()
-        console.log(posts)
-        res.status(200).json(posts)
+        const totalPosts = await PostModel.countDocuments({});
+        const totalPages = Math.ceil(totalPosts / limit); // Tính tổng số trang
+        const posts = await PostModel.find().skip(startIndex).limit(limit);
+
+        const pagination = {};
+        if (page < totalPages) {
+            pagination.next = {
+                page: page + 1,
+                limit: limit
+            };
+        }
+
+        if (page > 1) {
+            pagination.prev = {
+                page: page - 1,
+                limit: limit
+            };
+        }
+
+        res.status(200).json({ posts: posts, totalPages: totalPages, pagination: pagination });
     } catch (error) {
-        res.status(404).json({ message: error.message })
+        res.status(404).json({ message: error.message });
     }
 }
 
@@ -25,7 +47,7 @@ export const updatePosts = async (req, res) => {
     try {
         const updatedPost = req.body;
         const post = await PostModel.findByIdAndUpdate(
-            { _id: updatePosts._id },
+            req.params.id,
             updatedPost,
             { new: true }
         )
